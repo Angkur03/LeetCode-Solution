6ecc114ddcb68644ce94b9e134245001b31afee0
Angkur03 <74536923+Angkur03@users.noreply.github.com>
2022-05-18 00:14:38 +0600

    Time: 336 ms (48.50%), Space: 71.5 MB (37.91%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -3,11 +3,9 @@ public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>>pq;
         for(auto point: points) {
-            cout<<point[0]<<" "<<point[1]<<endl;
             int x = point[0];
             int y = point[1];
             int dis = abs(x*x) + abs(y*y);
-            //cout<<"dis "<<dis<<endl;
             pq.push({dis,{x,y}});
         }
         
@@ -15,12 +13,9 @@ public:
         while(!pq.empty() && k>0) {
             auto front = pq.top();
             pq.pop();
-            //cout<<"front "<<front.first<<" "<<front.second.first<<endl;
             ans.push_back({front.second.first,front.second.second});
             k--;
         }
         return ans;
-        //reverse(ans.begin(),ans.end());
-        //return {ans.begin(), ans.begin() + k};
     }
 };
