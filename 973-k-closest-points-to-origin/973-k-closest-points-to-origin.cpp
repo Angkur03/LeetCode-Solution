5857ad374239a62b2baad2943d84e4da2785827f
Angkur03 <74536923+Angkur03@users.noreply.github.com>
2022-05-17 23:45:12 +0600

    Time: 712 ms (12.16%), Space: 84 MB (14.27%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int,pair<int,int>> >pq;
+        for(auto point: points) {
+            cout<<point[0]<<" "<<point[1]<<endl;
+            int x = point[0];
+            int y = point[1];
+            int dis = abs(x*x) + abs(y*y);
+            //cout<<"dis "<<dis<<endl;
+            pq.push({dis,{x,y}});
+        }
+        
+        vector<vector<int>> ans;
+        while(!pq.empty() && k>0) {
+            auto front = pq.top();
+            pq.pop();
+            //cout<<"front "<<front.first<<" "<<front.second.first<<endl;
+            ans.push_back({front.second.first,front.second.second});
+        }
+        reverse(ans.begin(),ans.end());
+        return {ans.begin(), ans.begin() + k};
+    }
+};
